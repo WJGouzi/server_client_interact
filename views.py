477c03fa44d61900a113836e6f06242c957aecbc
gouzi <>
2018-08-09 22:49:21 +0800

    Post和Get 的区别以及文件上传的处理

--- views.py
+++ views.py
@@ -22,6 +22,17 @@ __create time__ = '2018/8/9'
 from django.shortcuts import render
 
 def homepage(req):
+    print('前端Get请求', req.GET)
+    print('前端Post请求', req.POST)
+    print('文件File发送', req.FILES)
+    # 循环去文件
+    for item in req.FILES:
+        print(item)
+        obj = req.FILES.get(item, None)
+        f = open(obj.name, 'wb')
+        for data_line in obj.chunks():
+            f.write(data_line)
+        f.close()
 
-    print('前端',req.GET)
+
     return render(req, "homepage.html")
