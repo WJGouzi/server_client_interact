9c5f7194db913f9580e5b133205a13375b58599d
gouzi <>
2018-08-09 22:22:09 +0800

    django 参与的前后端进行交互的demo

---
+++ views.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+__title__ = ''
+__author name__ = 'gouzi'
+__create time__ = '2018/8/9'
+# code is far away from bugs with the god animal protecting
+    I love animals. They taste delicious.
+             ┏┓   ┏┓
+            ┏┛┻━━━┛┻━━━┓
+            ┃    ☃    ┃
+            ┃  ┳┛  ┗┳  ┃
+            ┃    ┻     ┃
+            ┗━┓      ┏━┛
+              ┃      ┗━━━━━━━┓
+              ┃   神兽保佑    ┣┓
+              ┃    永无BUG！ ┏┛
+              ┗┓┓┏━━┳━┓ ┏━━━┛
+               ┃┫┫    ┃┫┫
+               ┗┻┛    ┗┻┛
+"""
+from django.shortcuts import render
+
+def homepage(req):
+
+    print('前端',req.GET)
+    return render(req, "homepage.html")
